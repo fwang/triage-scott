055dad1965a84c9c86baa5a40b5962f38837f047
Frank <wangfanjie@gmail.com>
2022-06-03 01:49:55 -0400

    Fix

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -3,34 +3,34 @@ import { Api, RDS, StackContext } from "@serverless-stack/resources";
 export function MyStack({ stack }: StackContext) {
   const DATABASE = "CounterDB";
 
-//  // Create the Aurora DB cluster
-//  const cluster = new RDS(stack, "Cluster", {
-//    engine: "postgresql10.14",
-//    defaultDatabaseName: DATABASE,
-//    migrations: "backend/migrations",
-//  });
+  // Create the Aurora DB cluster
+  const cluster = new RDS(stack, "Cluster", {
+    engine: "postgresql10.14",
+    defaultDatabaseName: DATABASE,
+    migrations: "backend/migrations",
+  });
 
   // Create a HTTP API
   const api = new Api(stack, "Api", {
-//    defaults: {
-//      function: {
-//        environment: {
-//          DATABASE,
-//          CLUSTER_ARN: cluster.clusterArn,
-//          SECRET_ARN: cluster.secretArn,
-//        },
-//        permissions: [cluster],
-//      },
-//    },
+    defaults: {
+      function: {
+        environment: {
+          DATABASE,
+          CLUSTER_ARN: cluster.clusterArn,
+          SECRET_ARN: cluster.secretArn,
+        },
+        permissions: [cluster],
+      },
+    },
     routes: {
       "POST /": "functions/lambda.handler",
     },
   });
 
-//  // Show the resource info in the output
-//  stack.addOutputs({
-//    ApiEndpoint: api.url,
-//    SecretArn: cluster.secretArn,
-//    ClusterIdentifier: cluster.clusterIdentifier,
-//  });
+  // Show the resource info in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+    SecretArn: cluster.secretArn,
+    ClusterIdentifier: cluster.clusterIdentifier,
+  });
 }
